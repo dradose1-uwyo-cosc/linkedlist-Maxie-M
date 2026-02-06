a78f461794c770781f936de3e998263b4ff8404e
Maxie <144727967+Maxie-M@users.noreply.github.com>
2026-02-05 20:25:21 -0700

    Update main.go
    Updated comments, added linked list, stack, and queue demos.

--- main.go
+++ main.go
@@ -1,56 +1,60 @@
-//[Your Name Here]
-//COSC 3750
-//[Date Here]
-//
-/*
-	Don't forget to run your go mod init command in your terminal
-	Review the assignment instructions for running your code
-	All the code you need to write should be put in the /ds/ package files
-	Uncomment the import statement for your module name
-	you can uncomment the tests in main as you go to test
-	The code in main is not an extensive test, you should add more and test your code as needed
-*/
-package main
+// main.go 
+// Maxie Machado 
+// Linked List 
+// Febuary 05, 2026 
+
+package main 
 
 import (
-	"fmt"
-	/*"hw01/ds" */ //this needs to be changes to your module name and uncommented
+	"fmt" 
+	"hw-linked/ds"
 )
 
 func main() {
-	fmt.Println("Only here so the import doesn't leave an error")
+	fmt.Println("=== LinkedList Demo ===")
+	var l ds.LinkedList
+	l.Insert("a")
+	l.Insert("b")
+	l.Insert("c")
+	l.PrintList()
 
-	//linkedlist := &ds.LinkedList{}
-	//linkedlist.InsertAt(0, "first")
-	//linkedlist.Insert("first")
-	//linkedlist.Insert("first")
-	//linkedlist.Insert("second")
-	//linkedlist.Insert("third")
-	//linkedlist.Insert("fourth")
-	//linkedlist.Insert("fifth")
-	//linkedlist.RemoveAt(4)
-	//linkedlist.PrintList()
-	//fmt.Println("The size of the linked list is:", linkedlist.GetSize())
-	//fmt.Println("-------------")
-	//linkedlist.RemoveAll("first")
-	//linkedlist.PrintList()
-	//fmt.Println("-------------")
-	//linkedlist.Reverse()
-	//linkedlist.PrintList()
-	//fmt.Println("The size of the linked list is:", linkedlist.GetSize())
-	//fmt.Println("-------------")
+	fmt.Println("\nInsertAt(1, \"x\")")
+	_ = l.InsertAt(1, "x") 
+	l.PrintList()
 
-	//stack := &ds.Stack{}
-	//stack.Push("first")
-	//stack.Push("second")
-	//stack.Push("third")
-	//data, _ := stack.Pop()
-	//println("Popped from stack:", data)
+	fmt.Println("\nRemove(\"b\")")
+	_ = l.Remove("b") 
+	l.PrintList()
 
-	//queue := &ds.Queue{}
-	//queue.Push("first")
-	//queue.Push("second")
-	//queue.Push("third")
-	//data, _ = queue.Pop()
-	//println("Popped from queue:", data)
+	fmt.Println("\nReverse()")
+	l.Reverse()
+	l.PrintList()
+
+	fmt.Println("\n=== Stack Demo ===") 
+	var s ds.Stack 
+	s.Push("one") 
+	s.Push("two")
+	s.Push("three") 
+
+	for {
+		v, ok := s.Pop()
+		if !ok {
+			break 
+		}
+		fmt.Println("popped:", v)
+	}
+
+	fmt.Println("\n=== Queue Demo ===")
+	var q ds.Queue 
+	q.Push("first") 
+	q.Push("second") 
+	q.Push("third") 
+	for i := 0; i < 4; i++ {
+		v,err := q.Pop()
+		if err != nil {
+			fmt.Println("pop error:", err)
+			break
+		}
+		fmt.Println("popped:", v)
+	}
 }
